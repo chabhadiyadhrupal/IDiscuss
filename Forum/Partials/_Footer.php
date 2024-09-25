05180589a70248ec865c21d03fd098b5c90ca389
Chabhadiya dhrupal <110007881+chabhadiyadhrupal@users.noreply.github.com>
2024-09-25 10:41:48 +0530

    Add files via upload

---
+++ Forum/Partials/_Footer.php
@@ -0,0 +1,7 @@
+<div class="container-fluid bg-dark text-light"  >
+<p class="text-center  pd-3 mb-0">
+    <!-- <marquee> -->
+    Copyright Idiscuss coading Forum 2021 || All Rights reserved
+    <!-- </marquee> -->
+</p>
+</div>
